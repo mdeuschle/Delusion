8fb69cdd19570d96133c3ed70a3bf2fd97fd0949
Matt Deuschle <mdeuschle@gmail.com>
2017-01-08 15:40:18 -0800

    Login custom class

---
+++ Delusion/RoundedButton.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedButton.swift
+//  Delusion
+//
+//  Created by Matt Deuschle on 1/8/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if let img = imageView {
+            img.contentMode = .scaleAspectFit
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 5
+    }
+
+}
