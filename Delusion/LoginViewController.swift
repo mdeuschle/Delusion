f3c491b0355064d1c8299d3d25280ad6afca2def
Matt Deuschle <mdeuschle@gmail.com>
2017-01-08 15:14:12 -0800

    Login UI

---
+++ Delusion/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Delusion
+//
+//  Created by Matt Deuschle on 1/8/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
