25e5b2dc22f0528b6fdb4dc3e3077affa9bf3978
Matt Deuschle <mdeuschle@gmail.com>
2017-01-08 17:13:20 -0800

    completed FB login

--- Delusion/LoginViewController.swift
+++ Delusion/LoginViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,5 +18,35 @@ class LoginViewController: UIViewController {
 
     }
 
+    @IBAction func fbButtonTapped(_ sender: RoundedButton) {
+
+        let fbLogin = FBSDKLoginManager()
+        fbLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if let res = result {
+                if error != nil {
+                    print("Not Able To Login To FB \(error?.localizedDescription)")
+                } else if res.isCancelled == true {
+                    print("User cancelled FB auth \(res.debugDescription)")
+                } else {
+                    print("Successfully authenticated with FB \(res)")
+                    let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                    self.fireBaseAuth(credential)
+                }
+            }
+        }
+    }
+
+    func fireBaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if let use = user {
+                if error != nil {
+                    print("Not able to authenticate with Firebase \(error?.localizedDescription)")
+                } else {
+                    print("Successfully authenticated with Firebase \(use.debugDescription)")
+                }
+            }
+        })
+    }
 
 }
+
